c0c888508dd23135b277572a367040685c001717
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2025-10-22 14:18:02 +0200

    feat(svgr): add and setup SVGR plugins, add ton svg icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+        svgoConfig: {
+          floatPrecision: 2,
+          plugins: [
+            {
+              name: "cleanupIds",
+            },
+            { name: "convertPathData" },
+          ],
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": "/src",
